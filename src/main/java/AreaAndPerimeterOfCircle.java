524681a6ec9ac4f8d79dfb44cccd9c9e38252123
vvinod <vvinod@tavisca.in>
2019-07-29 11:27:55 +0530

    Added area and perimeter classes which implements runnable and created two threads in main to run them individually.

--- src/main/java/AreaAndPerimeterOfCircle.java
+++ src/main/java/AreaAndPerimeterOfCircle.java
@@ -1,6 +1,34 @@
 public class AreaAndPerimeterOfCircle {
+
     public static void main(String [] a)
     {
-        System.out.println("Hello");
+        Area area = new Area(12);
+        Thread thread1 = new Thread(area);
+        thread1.start();
+        Perimeter perimeter = new Perimeter(12);
+        Thread thread2 = new Thread(perimeter);
+        thread2.start();
+    }
+}
+class Perimeter implements Runnable{
+    double radius;
+    public Perimeter(double r)
+    {
+        this.radius=r;
+    }
+    public void run(){
+        double perimeter=this.radius*3.14;
+        System.out.println("The area is "+perimeter*2);
+    }
+}
+class Area implements Runnable{
+    double radius;
+    public Area(double r)
+    {
+        this.radius=r;
+    }
+    public void run(){
+        double area=this.radius*this.radius;
+        System.out.println("The area is "+area*3.14);
     }
 }
